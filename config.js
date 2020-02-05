fc9356427b22f02e5e6c726c3b08556eab772175
Kamil Stangret <kamilstangret@wp.pl>
2020-02-05 12:37:20 +0100

    add config file

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  db: "" /*hasło do bazy danych*/,
+  db:
+    "mongodb+srv://kamils:uT8GmOJ0i3kW8R6C@cluster0-0j2j9.mongodb.net/test?retryWrites=true&w=majority" /*hasło do bazy danych*/,
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
